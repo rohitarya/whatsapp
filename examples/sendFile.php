e114397754bf82064e0ac3785705a147dc31e938
Rohit Arya <arya.rohit13@gmail.com>
2020-10-01 18:52:55 +0530

    Update sendFile.php

--- examples/sendFile.php
+++ examples/sendFile.php
@@ -5,7 +5,7 @@ $wa=new Whatsapp();
 $data['api_key']='XXXXXXXXXXXXX';  // Key received at the time of account creation
 $data['phone']='Receiver Mobile Number with country code and without + symbol';  // Receiver Mobile Number
 $data['message']='Your Message Here';  // Message to be send
-$data['filePathUrl']= 'File URL Here'; // Public URL of File
+$data['file_link']= 'File URL Here'; // Public URL of File
 $res=$wa->sendwhatsapp($data);
 echo "<pre>";
 print_r($data);
